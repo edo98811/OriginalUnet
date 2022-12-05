4545453ff8a648fe7d3428d2e9fbc6027b49bbdb
edo98811 <edoardofilippi77@gmail.com>
2022-12-05 10:50:08 +0000

    aggiunte cose

--- see_label.py
+++ see_label.py
@@ -5,16 +5,23 @@ import numpy as np
 
 
 def main():
-    data_dict = dm.load_dict()
+
+    # dm.list_files()
+
+    data_dict = dm.load_dict("img_and_annot.json")
 
     labels = data_dict["r_annot"]
 
-    label_img = dm.read_img(labels[0])
+    label_img_1 = dm.read_img(labels[15])
+    label_img_2 = dm.read_img(labels[4])
 
-    labels = np.unique(label_img.get_fdata())
+    labels_1 = np.unique(label_img_1.get_fdata())
+    labels_2 = np.unique(label_img_2.get_fdata())
 
-    print(labels)
+    for i in range(len(labels_1)):
+        print(f"{labels_1[i]}  {labels_2[i]}")
 
+    print(len(labels_1))
 
 
 if __name__ == "__main__":
